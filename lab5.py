3c0cddcc9150ac1579898b6235fc8c84d44d46f2
itamar20-meet <itamar20@meet.mit.edu>
2018-11-14 12:19:50 -0500

    lab_5

--- lab5.py
+++ lab5.py
@@ -132,8 +132,50 @@ a.print_info()
 
 b = Person("Jake",15, "pizza")
 b.define_color()
-b.print_info()'''
+b.print_info()
+
+class Bear():
+    def __init__(self, name):
+      self.name = name
+      print("A new bear created. Its name is: " + name)
+    
+    def say_hi(self, name):
+        print("Hi, my name is " + name)
+my_bear = Bear("Danny")
+print(my_bear.say_hi("Danny"))
 
 
+balloons = 5
+name = "Ron"
+color = "Yellow"
+
+print("This is a tale about " + str(balloons) + " balloons. The first kid is "  + name + " who got a " + color + "balloon")
 
 
+class Cake():
+    def __init__(self, flavor):
+        self.cake_flavor = flavor
+
+    def eat(self):
+        print("Yummy!!! Eating a" + self.cake_flavor + "cake :)")
+
+cake = Cake("chocolate")
+(cake.eat())
+# what I want to be printed: Yummy!!! Eating a chocolate cake :)
+
+class Cat():
+	def __init__(self,name,age):
+		self.name = name
+		self.age = 0 
+
+	def birthday(self):
+		self.age += 1
+		if self.age >= 100 :
+			print("Dong dong, the cat is dead!")
+		else:
+			print(self.name +" hasing its "+ str(self.age) + " birthday!")
+my_cat = Cat("Salem",8)
+my_cat.birthday()
+# what I want: my cat to celebrate its 8th birthday (and all the 
+# birthdays that come before that)
+'''
