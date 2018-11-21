d4ee7c6223bc29249476002f097bfaf7d5b2cc5c
itamar20-meet <itamar20@meet.mit.edu>
2018-11-21 12:02:40 -0500

    lab_6

--- lab6.py
+++ lab6.py
@@ -1,1 +1,19 @@
 import turtle 
+import random 
+from turtle import Turtle
+import colormode(255 )
+colors =("blue","green","red","orange","purple","yellow")
+class Square(Turtle):
+	"""docstring for square"""
+	def __init__(self, size,shape=turtle.shape("square")):
+		self.size = size
+
+	def random_color(self,red,blue,green):
+		color = random.choice(colors)
+		turtle.color(color)
+
+a = Square(10)
+
+a.random_color()
+
+turtle.mainloop()
