c715afb69626020cbd46249276d883fcf8074d09
itamar20-meet <itamar20@meet.mit.edu>
2019-01-22 10:23:25 -0500

    agar.io.py

--- agario.py
+++ agario.py
@@ -16,12 +16,12 @@ screen_height = turtle.getcanvas().winfo_height()/2
 
 BALLS = []
 
-my_ball = Ball(0,0,4,-1,50,"orange")
+my_ball = Ball(0,0,2,2,50,"red")
 
 
 NUMBER_OF_BALLS = 5
 MINIMUM_BALL_RADIUS = 10
-MAXIMUM_BALL_RADIUS = 100
+MAXIMUM_BALL_RADIUS = 60
 MINIMUM_BALL_DX = -5
 MAXIMUM_BALL_DX = 5
 MINIMUM_BALL_DY = -5
@@ -37,8 +37,11 @@ for i in range(NUMBER_OF_BALLS):
 	ball = Ball(x,y,dx,dy,radius,color)
 	BALLS.append(ball)
 
-for i in BALLS:
-	ball.move(400,300)
+turtle.update()
+def move_all_balls():
+
+	for i in BALLS:
+		i.move(screen_width,screen_height)
 
 def collide(ball1 ,ball2):
 
@@ -50,11 +53,11 @@ def collide(ball1 ,ball2):
 	d_between = math.sqrt(math.pow((x1 - x2),2) + math.pow((y1 - y2),2))
 	sum_radius = ball1.r + ball2.r
 	if ball1 == ball2:
-		return "false"
+		return False
 	elif sum_radius >= d_between:
-		return "True"
+		return True
 	else :
-		return "false"
+		return False
 def check_all_ball_collsion():
 	for ball_a in BALLS:
 		for ball_b in BALLS:
@@ -65,26 +68,55 @@ def check_all_ball_collsion():
 				y = random.randint(-screen_height + MAXIMUM_BALL_RADIUS, screen_height - MAXIMUM_BALL_RADIUS)
 				dx = random.randint(MINIMUM_BALL_DX, MAXIMUM_BALL_DX)
 				dy = random.randint(MINIMUM_BALL_DY, MAXIMUM_BALL_DY)
-				Radius = random.randint(MINIMUM_BALL_RADIUS, MAXIMUM_BALL_RADIUS)
-				colour =(random.random(), random.random(), random.random())
-				if radius1> radius2 :
+				radius = random.randint(MINIMUM_BALL_RADIUS, MAXIMUM_BALL_RADIUS)
+				color =(random.random(), random.random(), random.random())
+				if radius1 >= radius2 :
 					ball_a.r+= 1
-					ball_b = Ball(x, y, dx, dy, Radius, colour)
+					ball_b.x = x
+					ball_b.y = y
+					ball_b.r = radius
+					ball_b.penup()
+					ball_b.goto(x,y)
+					ball_b.color(color)
+					ball_b.shape("circle")
+					ball_b.shapesize(radius/10)
+					ball_b.dx = dx
+					ball_b.dy =dy
 				elif radius1< radius2 :
 					ball_b.r+= 1
-					ball_a = Ball(x, y, dx, dy, Radius, colour)
+					ball_a.x = x
+					ball_a.y = y
+					ball_a.r = radius
+					ball_a.penup()
+					ball_a.goto(x,y)
+					ball_a.color(color)
+					ball_a.shape("circle")
+					ball_a.shapesize(radius/10)
+					ball_a.dx = dx
+					ball_a.dy =dy
 
 def check_my_ball_collsion():
 	for ball_c in BALLS :
 		if collide(my_ball,ball_c):
 				radius3 = my_ball.r
 				radius4 = ball_c.r
-				if radius3 > radius4:
+				if radius3 <=radius4 :
+					 return False
+				else :
 					my_ball.r+= 1
-					ball_c = Ball(x, y, dx,dy, Radius, colour )
-				elif radius3 < radius4 :
-					return "false"
-	return "True"
+					ball_c.x = x
+					ball_c.y = y
+					ball_c.r = radius
+					ball_c.penup()
+					ball_c.goto(x,y)
+					ball_c.color(color)
+					ball_c.shape("circle")
+					ball_c.shapesize(radius/10)
+					ball_c.dx = dx
+					ball_c.dy =dy
+				
+	return True
+	
 			
 def movearound(event):
 	x = event.x - screen_width
@@ -95,9 +127,13 @@ turtle.getcanvas().bind("<Motion>", movearound)
 turtle.listen()
 
 
+while RUNNING == True:
+	screen_width = turtle.getcanvas().winfo_width()/2
+	screen_height = turtle.getcanvas().winfo_height()/2
+	move_all_balls()
+	check_all_ball_collsion()
+	RUNNING = check_my_ball_collsion()
+	turtle.update()
+	time.sleep(SLEEP)
 
-check_all_ball_collsion()
-check_my_ball_collsion()
 
-
-turtle.mainloop()
--- ball.py
+++ ball.py
@@ -7,8 +7,8 @@ class Ball(Turtle):
 	"""docstring for Ball"""
 	def __init__(self, x, y ,dx, dy,r, color):
 		Turtle.__init__(self)
-		turtle.penup()
-		turtle.goto(x, y)
+		self.penup()
+		self.goto(x, y)
 		self.x = x
 		self.y = y
 		self.dx = dx
@@ -16,28 +16,31 @@ class Ball(Turtle):
 		self.r = r
 		self.color(color)
 		self.shape("circle")
-		turtle.shapesize(r/10)
-		
+		self.shapesize(r/10)
+		self.showturtle()
 	def move(self,screen_width, screen_height):
 			current_x = self.xcor()
 			new_x = current_x + self.dx
 			current_y = self.ycor()
 			new_y = current_y + self.dy
 			right_ball_side = new_x + self.r
-			left_ball_side = new_x + self.r
+			left_ball_side = new_x - self.r
 			up_ball_side = new_y + self.r
-			down_ball_side = new_y + self.r
-			if screen_height == up_ball_side :
-				dy = dy-5
-			elif screen_height == down_ball_side*(-1):
-				dy = dy-5
-			elif screen_width == left_ball_side*(-1):
-				dx = dx+5
-			elif screen_width == right_ball_side:
-				dx = dx+5
-			else:
-				pass
+			down_ball_side = new_y - self.r
+			self.goto(new_x,new_y)
+			if screen_height <= up_ball_side :
+				self.dy = -self.dy
+			elif -screen_height >= down_ball_side:
+				self.dy = -self.dy
+			elif -screen_width >= left_ball_side:
+				self.dx = -self.dx
+			elif screen_width <= right_ball_side:
+				self.dx = -self.dx
+
 			
 
-
+'''my_ball = Ball(0,0,2,2,50,"red")
+while True:
+	my_ball.move(200,200)
+turtle.mainloop()'''
 		
